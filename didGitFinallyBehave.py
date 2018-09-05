7519e101fe8918f6724eb26e7cf67e3292466b65
rlsharpton <robin@robinls.net>
2018-09-05 14:49:16 -0500

    will git be nice to me finally?

---
+++ didGitFinallyBehave.py
@@ -0,0 +1,8 @@
+#! python3
+__author__ = 'rls'
+__version__ = 0.1
+''' Program written by Robin Sharpton
+    rest of DocString goes here 
+'''
+
+print("hello github are you finally going to play nice with me??")
