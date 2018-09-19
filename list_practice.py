2836d568d632fe2e9d6284cb4abaa6ebd627c83f
rlsharpton <robin@robinls.net>
2018-09-19 17:11:46 -0500

    lists on codeacademy

--- list_practice.py
+++ list_practice.py
@@ -17,3 +17,28 @@ print(clist)
 print(dlist)
 dlist.remove(6)
 print(dlist)
+
+n = [3, 5, 7]
+
+def total(numbers):
+    result = 0
+    for i in range(len(numbers)):
+        result = result + numbers[i]
+
+    return result
+
+print(total(n))
+
+n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
+
+
+# Add your function here
+def flatten(lists):
+    results = []
+    for lst in lists:
+        for item in lst:
+            results.append(item)
+
+    return results
+
+print(flatten(n))
--- student_becomes_teacher.py
+++ student_becomes_teacher.py
@@ -4,4 +4,45 @@ __version__ = 0.1
 ''' Program written by Robin Sharpton
     rest of DocString goes here 
 '''
-lloyd
+
+lloyd = {
+  "name": "Lloyd",
+  "homework": [90.0, 97.0, 75.0, 92.0],
+  "quizzes": [88.0, 40.0, 94.0],
+  "tests": [75.0, 90.0]
+}
+alice = {
+  "name": "Alice",
+  "homework": [100.0, 92.0, 98.0, 100.0],
+  "quizzes": [82.0, 83.0, 91.0],
+  "tests": [89.0, 97.0]
+}
+tyler = {
+  "name": "Tyler",
+  "homework": [0.0, 87.0, 75.0, 22.0],
+  "quizzes": [0.0, 75.0, 78.0],
+  "tests": [100.0, 100.0]
+}
+
+# Add your function below!
+def average(numbers):
+  total = sum(numbers)
+  total = float(total)
+  return total/len(numbers)
+
+def get_average(student):
+  homework = average(student["homework"])
+  quizzes = average(student["quizzes"])
+  tests = average(student["tests"])
+  return 0.1 * homework + 0.3 * quizzes + 0.6 * tests
+
+class_list = [lloyd, alice, tyler]
+
+def get_class_average(studs):
+  results = []
+  for li in studs:
+    results.append(get_average(li))
+  #return
+  #.append(get_average(class_list))
+
+get_class_average(class_list)
