c60f0bcdf322e2eca0519152290c305b67d87c08
rlsharpton <robin@robinls.net>
2018-10-07 15:29:47 -0500

    Sunday

---
+++ battleship.py
@@ -0,0 +1,16 @@
+#! python3
+__author__ = 'rls'
+__version__ = 0.1
+''' Program written by Robin Sharpton
+    rest of DocString goes here 
+'''
+
+board = []
+for outer in range(5):
+    x = print(["O"] * 5)
+    board.append(x)
+
+print("\n" * 2)
+print(board[3])
+#board.remove(y)
+#print(board)
